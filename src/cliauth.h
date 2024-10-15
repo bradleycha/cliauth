71cfa01b5261ad92f6a53fc3d87e4a1812e1430f
Chase Bradley <bradleycha@proton.me>
2024-10-15 16:13:17 -0400

    Add macro for halting and unreachable code branches, add documentation to cliauth.h

--- src/cliauth.h
+++ src/cliauth.h
@@ -13,8 +13,66 @@
 /* times */
 #include "config.h"
 
-#define _CLIAUTH_NULLPTR_SENTINEL   (0u)
-#define CLIAUTH_NULLPTR             ((void *)_CLIAUTH_NULLPTR_SENTINEL)
+/* additional config options which require more advanced code to set properly */
+
+#if defined(__clang__)
+#define CLIAUTH_CONFIG_PLATFORM_COMPILER_IS_GCC    0
+#define CLIAUTH_CONFIG_PLATFORM_COMPILER_IS_CLANG  1
+#define CLIAUTH_CONFIG_PLATFORM_COMPILER_IS_MSVC   0
+#elif defined(__GNUC__) || defined(__GNUG__)
+#define CLIAUTH_CONFIG_PLATFORM_COMPILER_IS_GCC    1
+#define CLIAUTH_CONFIG_PLATFORM_COMPILER_IS_CLANG  0
+#define CLIAUTH_CONFIG_PLATFORM_COMPILER_IS_MSVC   0
+#elif defined(_MSC_VER)
+#define CLIAUTH_CONFIG_PLATFORM_COMPILER_IS_GCC    0
+#define CLIAUTH_CONFIG_PLATFORM_COMPILER_IS_CLANG  0
+#define CLIAUTH_CONFIG_PLATFORM_COMPILER_IS_MSVC   1
+#endif
+
+#define _CLIAUTH_NULLPTR_SENTINEL\
+   (0u)
+
+/*----------------------------------------------------------------------------*/
+/* Generic null pointer value which can be used for any pointer type.         */
+/*----------------------------------------------------------------------------*/
+#define CLIAUTH_NULLPTR\
+   ((void *)_CLIAUTH_NULLPTR_SENTINEL)
+
+/*----------------------------------------------------------------------------*/
+/* Halts execution without terminating the program.  This is effectively the  */
+/* same as an infinite loop.                                                  */
+/*----------------------------------------------------------------------------*/
+#define CLIAUTH_HALT\
+   {for (;;) {}}
+
+#if CLIAUTH_CONFIG_PLATFORM_COMPILER_IS_GCC
+#if __has_builtin(__builtin_unreachable)
+#define _CLIAUTH_UNREACHABLE_BUILTIN __builtin_unreachable()
+#endif /* __has_builtin(__builtin_unreachable) */
+#endif /* CLIAUTH_CONFIG_PLATFORM_COMPILER_IS_GCC */
+#if CLIAUTH_CONFIG_PLATFORM_COMPILER_IS_CLANG
+#if __has_builtin(__builtin_unreachable)
+#define _CLIAUTH_UNREACHABLE_BUILTIN __builtin_unreachable()
+#endif /* __has_builtin(__builtin_unreachable) */
+#endif /* CLIAUTH_CONFIG_PLATFORM_COMPILER_IS_CLANG */
+#if CLIAUTH_CONFIG_PLATFORM_COMPILER_IS_MSVC
+#define _CLIAUTH_UNREACHABLE_BUILTIN __assume(0)
+#endif /* CLIAUTH_CONFIG_PLATFORM_COMPILER_IS_MSVC */
+
+/* fallback unreachable implementation when no builtin was able to be */
+/* detected for the given compiler */
+#ifndef _CLIAUTH_UNREACHABLE_BUILTIN
+#define _CLIAUTH_UNREACHABLE_BUILTIN CLIAUTH_HALT
+#endif /* _CLIAUTH_UNREACHABLE_BUILTIN */
+
+/*----------------------------------------------------------------------------*/
+/* Tells the compiler that a code branch will never be reached.  It is        */
+/* undefined behavior to reach an unreachable code branch.  This can be used  */
+/* to aid in optimization as well as eliminate spurious warnings, such as in  */
+/* the case of initialization of variables in a switch statement.             */
+/*----------------------------------------------------------------------------*/
+#define CLIAUTH_UNREACHABLE\
+   _CLIAUTH_UNREACHABLE_BUILTIN
 
 /* implicit includes are at the bottom to ensure all previous project */
 /* declarations are available in these headers */
